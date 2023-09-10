9edf3d1e69c9067e38a173f0b04494b5c303d3f0
pankaj485 <pankajsamar2@gmail.com>
2023-09-10 13:46:19 +0530

    mobile: minor css fixes

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -8,8 +8,10 @@ const Info = () => {
 					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
 						<h3 className="font-Fraunces text-xl text-Dark-Purple ">1</h3>
 					</div>
-					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Actionable insights</h2>
-					<p className="leading-7 text-Davys-Gray">
+					<h2 className="font-Fraunces font-semibold text-2xl my-6 text-Dark-Purple">
+						Actionable insights
+					</h2>
+					<p className="leading-8 text-lg text-Davys-Gray px-4 text-center">
 						Optimize your products, improve customer satisfaction and stay ahead of the competition
 						with our product data analytics.
 					</p>
@@ -19,8 +21,10 @@ const Info = () => {
 					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
 						<h3 className="font-Fraunces text-xl text-Dark-Purple ">2</h3>
 					</div>
-					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Data-driven decisions</h2>
-					<p className="leading-7 text-Davys-Gray">
+					<h2 className="font-Fraunces font-semibold text-2xl my-6 text-Dark-Purple">
+						Data-driven decisions
+					</h2>
+					<p className="leading-8 text-lg text-Davys-Gray px-4 text-center">
 						Make data-driven decisions with our product data analytics. Our AI-generated reports
 						help you unlock insights hidden in your product data.
 					</p>
@@ -30,8 +34,10 @@ const Info = () => {
 					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
 						<h3 className="font-Fraunces text-xl text-Dark-Purple ">3</h3>
 					</div>
-					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Always affordable</h2>
-					<p className="leading-7 text-Davys-Gray">
+					<h2 className="font-Fraunces font-semibold text-2xl my-6 text-Dark-Purple">
+						Always affordable
+					</h2>
+					<p className="leading-8 text-lg text-Davys-Gray px-4 text-center">
 						Always affordable pricing that scales with your business. Get top-quality product data
 						analytics services without hidden costs or unexpected fees.
 					</p>
--- src/components/promotion/Promotion.jsx
+++ src/components/promotion/Promotion.jsx
@@ -2,14 +2,14 @@ const Promotion = () => {
 	return (
 		<div className="promotionBlock mx-4 mt-24">
 			<img className="px-8" src="/assets/images/image-founder.webp" alt="founder image" />
-			<div className="relative bg-Dark-Purple text-white p-8 text-center -mt-10 ">
-				<h2 className="font-Fraunces text-3xl font-semibold mb-4">Be the first to test</h2>
-				<p className="text-base leading-7 ">
+			<div className="relative bg-Dark-Purple text-white p-8 text-center -mt-12 ">
+				<h2 className="font-Fraunces text-3xl font-semibold mb-5">Be the first to test</h2>
+				<p className="leading-8 text-lg px-4 text-center">
 					Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a
 					beta tester for our app and kickstart your company. Apply for access below and I'll be in
 					touch to schedule a call.
 				</p>
-				<button className="bg-Eucaplyptus px-6 py-3 mt-6 mb-8  font-bold text-Dark-Purple leading-buttonFontLineHeight tracking-navbarCustomSpacing hover:cursor-pointer border-2 border-transparent active:border-2 active:border-Eucaplyptus active:bg-transparent active:text-Eucaplyptus ">
+				<button className="bg-Eucaplyptus px-7 py-4 mt-6 mb-8 text-xl font-bold text-Dark-Purple leading-buttonFontLineHeight tracking-navbarCustomSpacing hover:cursor-pointer border-2 border-transparent active:border-2 active:border-Eucaplyptus active:bg-transparent active:text-Eucaplyptus ">
 					Apply for access
 				</button>
 			</div>
