72586b7928a9766f3acfb26c21a5164611a5c257
pankaj485 <pankajsamar2@gmail.com>
2023-09-10 12:28:58 +0530

    mobile: add info section

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,32 +1,44 @@
+import "./info.css";
+
 const Info = () => {
 	return (
-		<div className="bg-Ghost-White ">
-			<div>
-				<h3>1</h3>
-				<h2>Actionable insights</h2>
-				<p>
-					Optimize your products, improve customer satisfaction and stay ahead of the competition
-					with our product data analytics.
-				</p>
+		<div className="">
+			<div className="bg-Ghost-White flex flex-col justify-center items-center gap-10 px-4 pt-16">
+				<div className="infoCard flex flex-col items-center justify-center">
+					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
+						<h3 className="font-Fraunces text-xl text-Dark-Purple ">1</h3>
+					</div>
+					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Actionable insights</h2>
+					<p className="leading-7 text-Davys-Gray">
+						Optimize your products, improve customer satisfaction and stay ahead of the competition
+						with our product data analytics.
+					</p>
+				</div>
+
+				<div className="infoCard flex flex-col items-center justify-center">
+					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
+						<h3 className="font-Fraunces text-xl text-Dark-Purple ">2</h3>
+					</div>
+					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Data-driven decisions</h2>
+					<p className="leading-7 text-Davys-Gray">
+						Make data-driven decisions with our product data analytics. Our AI-generated reports
+						help you unlock insights hidden in your product data.
+					</p>
+				</div>
+
+				<div className="infoCard flex flex-col items-center justify-center">
+					<div className="flex items-center justify-center rounded-full border border-1 h-10 w-10 p-6 border-Davys-Gray">
+						<h3 className="font-Fraunces text-xl text-Dark-Purple ">3</h3>
+					</div>
+					<h2 className="font-Fraunces text-2xl my-6 text-Dark-Purple">Always affordable</h2>
+					<p className="leading-7 text-Davys-Gray">
+						Always affordable pricing that scales with your business. Get top-quality product data
+						analytics services without hidden costs or unexpected fees.
+					</p>
+				</div>
 			</div>
 
-			<div>
-				<h3>2</h3>
-				<h2>Data-driven decisions</h2>
-				<p>
-					Make data-driven decisions with our product data analytics. Our AI-generated reports help
-					you unlock insights hidden in your product data.
-				</p>
-			</div>
-
-			<div>
-				<h3>3</h3>
-				<h2>Always affordable</h2>
-				<p>
-					Always affordable pricing that scales with your business. Get top-quality product data
-					analytics services without hidden costs or unexpected fees.
-				</p>
-			</div>
+			<div className="infoShape after:bg-Ghost-White"></div>
 		</div>
 	);
 };
